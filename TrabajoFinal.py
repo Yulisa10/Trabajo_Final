23bb6b5e24a322abfd5073721849556595c23f48
Yulisa10 <yortizg@unal.edu.co>
2025-02-10 23:29:44 -0500

    Update TrabajoFinal.py

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -252,11 +252,9 @@ elif seccion == "Modelo de redes neuronales":
 
     def load_model():
         """Cargar el modelo y sus pesos desde el archivo model_weights.pkl."""
-        filename = 'model.pkl.gz'
-        with gzip.open(filename, 'rb') as f:
-            model = pickle.load(f)
-        return model
-    
+filename = "model.pkl.gz"
+with gzip.open(filename, "rb") as f:
+    model = pickle.load(f)
     model = load_model()
     model.compile(loss='binary_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
